c2b56bd46ae37557ae325fe85f8037102352b14c
DS <DS@DS-Komputer>
2017-02-06 19:16:33 +0100

    initial checkin

---
+++ com.secretofglory/src/main/java/testowka/App.java
@@ -0,0 +1,17 @@
+package testowka;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+		 new ChromeDriver();
+    }
+}
---
+++ com.secretofglory/src/test/java/testowka/Test1.java
@@ -0,0 +1,59 @@
+package testowka;
+
+import static org.testng.Assert.fail;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+
+	static WebDriver driver;
+
+	@BeforeClass
+	public void setUp() {
+		
+		System.setProperty("webdriver.gecko.driver", "D:\\Danusia\\Testowanie\\geckodriver-v0.14.0-win64\\geckodriver.exe");
+		driver = new FirefoxDriver();
+//		driver = new ChromeDriver();
+	}
+
+	@Test
+	public void ShouldBePosibleToSearchTestowkaAtGoogle() throws InterruptedException {
+		driver.get("https://www.google.pl/?gws_rd=ssl");
+		WebElement searchField = driver.findElement(By.name("q"));
+		searchField.sendKeys("testowka.pl");
+		
+		WebElement searchButton = driver.findElement(By.xpath("//*[@id=lst-ib]/span"));
+		searchButton.click();
+		for (int second = 0;; second++) {
+			if (second >= 60)
+				fail("timeout");
+			try {
+				if (driver.findElement(By.linkText("Testowka.pl")).isDisplayed()) {
+					break;
+				}
+			} catch (Exception e) {
+			}
+			Thread.sleep(1000);
+		}
+		WebElement linkToTestowka = driver.findElement(By.linkText("Testowka.pl"));
+		linkToTestowka.click();
+		
+		
+		
+		
+	}
+
+	@AfterClass
+	public static void tearDown() {
+		driver.close();
+	}
+
+}
