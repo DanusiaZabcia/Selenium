78d38ff1b84d1c0a01c5697f58442980bf12bf46
DS <DS@DS-Komputer>
2017-02-19 11:08:06 +0100

    Second Test with before method

--- com.secretofglory/src/test/java/testowka/Selenium1.java
+++ com.secretofglory/src/test/java/testowka/Selenium1.java
@@ -1,12 +1,21 @@
 package testowka;
 
+import static junit.framework.Assert.assertTrue;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-
-import static junit.framework.Assert.assertTrue;
 public class Selenium1 {
 
+	
+	@Before
+	
+	public void before(){
+		System.setProperty("webdriver.gecko.driver", "D:\\Danusia\\Testowanie\\geckodriver-v0.14.0-win64\\geckodriver.exe");
+	}
+	
 	@Test
 	
 	public void driveIsTheKing(){
@@ -15,7 +24,21 @@ public class Selenium1 {
 		
 		driver.get("http://compendiumdev.co.uk/selenium");
 		
-		assertTrue(driver.getTitle().startsWith("Selenium Ximplified"));
+		assertTrue(driver.getTitle().startsWith("Selenium Simplified"));
+			
+	}
+	
+	@Test
+	
+public void firefoxIsSupportedByWebdriver(){
+		
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.get("http://compendiumdev.co.uk/selenium");
+		
+		assertTrue(driver.getTitle().startsWith("Selenium Simplified"));
+		
+		driver.quit();
 			
 	}
 }
